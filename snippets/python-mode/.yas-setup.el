a437960207eb26d6a80b8e60b00301f726b1951c
Gustaf Waldemarson <gustaf.waldemarson@gmail.com>
2021-08-30 18:00:58 +0200

    Adding initial set of Python snippets.
    - ReST Style:
      - Class docstring
      - Function docstring
      - "Update" docstring
    
    - Google Style:
      - Function Docstring

---
+++ snippets/python-mode/.yas-setup.el
@@ -0,0 +1,90 @@
+;;; python-mode/.yas-setup.el --- yasnippet setup for python-mode.
+;;
+;;; Commentary:
+;;
+;; Setup code for Python based yasnippets.
+;;
+;;; Code:
+
+
+(defun python-split-args (arg-string)
+  "Split the python ARG-STRING into ((name, default)..) tuples."
+  (mapcar (lambda (x)
+             (split-string x "[[:blank:]]*=[[:blank:]]*" t))
+          (split-string arg-string "[[:blank:]]*,[[:blank:]]*" t)))
+
+
+(defun python-args-to-reST-docstring (text &optional make-fields)
+  "Return a reST docstring format for the python arguments in TEXT.
+
+Optional argument MAKE-FIELDS will create yasnippet compatible
+field that the can be jumped to upon further expansion."
+  (let* ((indent (concat "\n" (make-string (current-column) 32)))
+         (args (python-split-args text))
+	 (nr 0)
+         (formatted-args
+	  (mapconcat
+	   (lambda (x)
+	     (concat ":param " (nth 0 x) ":"
+		     (if make-fields (format " ${%d:arg%d}" (cl-incf nr) nr))
+		     (if (nth 1 x) (concat " \(default " (nth 1 x) "\)"))))
+	   args
+	   indent)))
+    (unless (string= formatted-args "")
+      (concat
+       indent
+       (mapconcat 'identity
+		  (list ".. Keyword Arguments:" formatted-args)
+		  indent)
+       indent))))
+
+
+(defun python-arg-types-to-reST-docstring (text &optional make-fields)
+  "Return a ReST docstring format for the Python arguments in TEXT.
+
+Optional argument MAKE-FIELDS will create yasnippet compatible
+field that the can be jumped to upon further expansion."
+  (let* ((indent (concat "\n" (make-string (current-column) 32)))
+         (args (python-split-args text))
+	 (i 0)
+	 (nr (length args))
+	 (formatted-types
+	  (mapconcat (lambda (x)
+		       (concat ":type " (nth 0 x) ":"
+			       (if make-fields
+				   (format " ${%d:type%d}"
+					   (cl-incf nr) (cl-incf i)))))
+		     args indent)))
+    (unless (string= formatted-types "")
+      (concat
+       (mapconcat 'identity (list  ".. Types:" formatted-types) indent)
+       indent))))
+
+
+(defun python-args-to-google-docstring (text &optional make-fields)
+  "Return a Google docstring for the Python arguments in TEXT.
+
+Optional argument MAKE-FIELDS will create yasnippet compatible
+field that the can be jumped to upon further expansion."
+  (let* ((indent (concat "\n" (make-string (current-column) 32)))
+         (args (python-split-args text))
+    	 (nr 0)
+         (formatted-args
+    	  (mapconcat
+    	   (lambda (x)
+    	     (concat "   " (nth 0 x)
+    		     (if make-fields (format " ${%d:arg%d}" (cl-incf nr) nr))
+    		     (if (nth 1 x) (concat " \(default " (nth 1 x) "\)"))))
+    	   args
+    	   indent)))
+    (unless (string= formatted-args "")
+      (concat
+       (mapconcat 'identity
+    		  (list "" "Args:" formatted-args)
+    		  indent)
+       "\n"))))
+
+
+(provide 'python-mode/.yas-setup.el)
+
+;;; .yas-setup.el ends here
---
+++ yasnippet-radical-snippets.el
@@ -0,0 +1,75 @@
+;;; yasnippet-radical-snippets.el --- Collection of radical yasnippet snippets
+
+;; Copyright (C) 2021 Gustaf Waldemarson
+
+;; Author: Gustaf Waldemarson <gustaf.waldemarson@gmail.com>
+;; Version: 0.1
+;; Package-Requires: ((yasnippet "0.8.0"))
+;; Keywords: convenience, snippets
+;; Homepage: https://github.com/Xaldew/yasnippet-radical-snippets
+
+;;; Commentary:
+
+;; This repository contains a small collection of *radical* snippets for the
+;; yasnippet package.
+
+;;; License:
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Code:
+
+(require 'yasnippet)
+
+
+(defconst yasnippet-radical-snippets-dir
+  (expand-file-name
+   "snippets"
+   (file-name-directory
+    ;; Copied from ‘f-this-file’ from f.el.
+    (cond
+     (load-in-progress load-file-name)
+     ((and (boundp 'byte-compile-current-file) byte-compile-current-file)
+      byte-compile-current-file)
+     (:else (buffer-file-name))))))
+
+
+;;;###autoload
+(defun yasnippet-radical-snippets-initialize ()
+  "Load the `yasnippet-snippets' snippets directory."
+  ;; NOTE: we add the symbol `yasnippet-radical-snippets-dir' rather than its
+  ;; value, so that yasnippet will automatically find the directory
+  ;; after this package is updated (i.e., moves directory).
+  (add-to-list 'yas-snippet-dirs 'yasnippet-snippets-dir t)
+  (yas-load-directory yasnippet-radical-snippets-dir t))
+
+
+(defgroup yasnippet-radical-snippets nil
+  "Options for yasnippet setups.
+
+This is useful for customizing options declared in
+“.yas-setup.el” files.  For example, you could declare a
+customizable variable used for a snippet expansion.  See Info
+node `(elisp)Customization Types'."
+  :group 'yasnippet)
+
+
+;;;###autoload
+(eval-after-load 'yasnippet
+  '(yasnippet-radical-snippets-initialize))
+
+
+(provide 'yasnippet-radical-snippets)
+
+;;; yasnippet-radical-snippets.el ends here
